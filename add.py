90c7c9df0c94921e7d452cc5b83f79e046a3824c
Rhys Lloyd <rrl1n25@soton.ac.uk>
2025-11-24 09:39:12 +0000

    Commented out arguement check in function 'add'

--- add.py
+++ add.py
@@ -1,5 +1,5 @@
 def add(x: int, y: int) -> int:
     """Returns the sum of two integers."""
-    if not isinstance(x, int) or not isinstance(y, int):
-        raise TypeError
+    #if not isinstance(x, int) or not isinstance(y, int):
+    #    raise TypeError
     return x + y
