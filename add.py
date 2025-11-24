72443fb78a016ecef3b00b26ebaa664a89c5d9d8
Rhys Lloyd <rrl1n25@soton.ac.uk>
2025-11-24 09:47:15 +0000

    Uncommented the arg check for func 'add'

--- add.py
+++ add.py
@@ -1,6 +1,6 @@
 def add(x: int, y: int) -> int:
     """Returns the sum of two integers."""
-    #if not isinstance(x, int) or not isinstance(y, int):
-    #    raise TypeError
+    if not isinstance(x, int) or not isinstance(y, int):
+        raise TypeError
     return x + y
 
